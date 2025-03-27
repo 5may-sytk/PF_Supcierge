1129f60b523da9bcee2974cd6ce718943a31af37
5may-sytk <189351572 +5may-sytk@users.noreply.github.com>
2025-03-27 03:55:47 +0000

    Top/About作成

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,10 @@ Rails.application.routes.draw do
     sessions: 'public/sessions',
     passwords: 'public/passwords'
   }
+
+  root to: "homes#top"
+  get "about", to: "homes#about"
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
