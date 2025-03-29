a546ec42d032190a1b14d89f1dad2f6dedc19808
5may-sytk <189351572 +5may-sytk@users.noreply.github.com>
2025-03-29 02:43:15 +0000

    サインインページ、ヘッダー・フッター調整

--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,21 @@ Rails.application.routes.draw do
     sessions: 'admin/sessions',
   }
 
-  devise_for :users,skip: [:mailer], controllers: {
+  devise_for :users, skip: [:mailer, :passwords], controllers: {
     registrations: "public/registrations",
-    sessions: 'public/sessions',
-    passwords: 'public/passwords'
+    sessions: 'public/sessions'
+    }, path_names: {
+      sign_in: 'Log_in'
   }
 
   root to: "homes#top"
   get "about", to: "homes#about"
 
+  namespace :admin do
+  end
+
+  scope module: :public do
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
